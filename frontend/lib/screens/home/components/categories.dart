dc56b62ab685cc38eba5aa6e5a76d5b47a68177c
Mareeha Malik <125895497+mareeha-malik@users.noreply.github.com>
2025-02-16 18:29:38 +0500

    Added categories.dart

--- frontend/lib/screens/home/components/categories.dart
+++ frontend/lib/screens/home/components/categories.dart
@@ -53,7 +53,7 @@ class CategoryCard extends StatelessWidget {
             height: 56,
             width: 56,
             decoration: BoxDecoration(
-              color: const Color(0xFFFFECDF),
+              color:  const Color(0xFF181818),
               borderRadius: BorderRadius.circular(10),
             ),
             child: SvgPicture.asset(icon),
