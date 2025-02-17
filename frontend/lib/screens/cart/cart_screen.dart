77fafd0f9145e3a67ab3d744f0667bada2446202
Mareeha Malik <125895497+mareeha-malik@users.noreply.github.com>
2025-02-17 22:58:52 +0500

    Update cart_screen.dart

--- frontend/lib/screens/cart/cart_screen.dart
+++ frontend/lib/screens/cart/cart_screen.dart
@@ -1,70 +1,16 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'Product.dart';
 
-import '../../models/Cart.dart';
-import 'components/cart_card.dart';
-import 'components/check_out_card.dart';
+class Cart {
+  final Product product;
+  final int numOfItem;
 
-class CartScreen extends StatefulWidget {
-  static String routeName = "/cart";
-
-  const CartScreen({super.key});
-
-  @override
-  State<CartScreen> createState() => _CartScreenState();
+  Cart({required this.product, required this.numOfItem});
 }
 
-class _CartScreenState extends State<CartScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Column(
-          children: [
-            const Text(
-              "Your Cart",
-              style: TextStyle(color: Colors.black),
-            ),
-            Text(
-              "${demoCarts.length} items",
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-          ],
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: ListView.builder(
-          itemCount: demoCarts.length,
-          itemBuilder: (context, index) => Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: Dismissible(
-              key: Key(demoCarts[index].product.id.toString()),
-              direction: DismissDirection.endToStart,
-              onDismissed: (direction) {
-                setState(() {
-                  demoCarts.removeAt(index);
-                });
-              },
-              background: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                decoration: BoxDecoration(
-                  color: const Color(0xFFFFE6E6),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Row(
-                  children: [
-                    const Spacer(),
-                    SvgPicture.asset("assets/icons/Trash.svg"),
-                  ],
-                ),
-              ),
-              child: CartCard(cart: demoCarts[index]),
-            ),
-          ),
-        ),
-      ),
-      bottomNavigationBar: const CheckoutCard(),
-    );
-  }
-}
+// Demo data for our cart
+
+List<Cart> demoCarts = [
+  Cart(product: demoProducts[0], numOfItem: 2),
+  Cart(product: demoProducts[1], numOfItem: 1),
+  Cart(product: demoProducts[3], numOfItem: 1),
+];
