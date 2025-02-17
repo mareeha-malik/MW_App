cde2df71031d60706a38c4144eb2e6b20a32f42a
AwaisAli3101 <maalik.awais.310@gmail.com>
2025-02-17 23:01:14 +0500

    Update profile_screen.dart

--- frontend/lib/screens/profile/profile_screen.dart
+++ frontend/lib/screens/profile/profile_screen.dart
@@ -10,7 +10,9 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor:Colors.black,
       appBar: AppBar(
+        backgroundColor: Colors.black,
         title: const Text("Profile"),
       ),
       body: SingleChildScrollView(
